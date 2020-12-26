d2d35216a172249857aecfc9c4c2543f3e1199dc
OscarDOM <dominguez.celada@gmail.com>
2020-12-26 22:43:12 +0100

    refactor(puppeteer): wrap login and addHours steps inside functions (#52)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const puppeteer = require('puppeteer')
 
+const bambooLogin = async (page) => {
+  await page.goto(`https://${process.env.COMPANY}.bamboohr.com/login.php`)
+  await page.click('.normal-login-link-container')
+  await page.type('#lemail', process.env.USER)
+  await page.type('input[type="password"]', process.env.PASSWORD)
+  const [response] = await Promise.all([
+    page.waitForNavigation(),
+    page.click('[type="submit"]')
+  ])
+
+  return response
+}
+
+const addWorkingHoursToDay = (page) => {
+  await page.type('.TimeTrackingWidget__form input', '8')
+  await page.click('.js-save-timesheet-button-wrap button')
+} 
+
 async function main () {
   const browser = await puppeteer.launch({
     headless: true,
@@ -8,17 +26,11 @@ async function main () {
   })
   const page = await browser.newPage()
   try {
-    await page.goto(`https://${process.env.COMPANY}.bamboohr.com/login.php`)
-    await page.click('.normal-login-link-container')
-    await page.type('#lemail', process.env.USER)
-    await page.type('input[type="password"]', process.env.PASSWORD)
-    const [response] = await Promise.all([
-      page.waitForNavigation(),
-      page.click('[type="submit"]')
-    ])
+    const response = await bambooLogin(page)
     console.log(response)
-    await page.type('.TimeTrackingWidget__form input', '8')
-    await page.click('.js-save-timesheet-button-wrap button')
+
+    await addWorkingHoursToDay(page)
+    
     browser.close()
   } catch (error) {
     console.error(error)
