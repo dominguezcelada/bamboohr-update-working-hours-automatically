403b791b9b6074e73aea772b4322e9824c942f72
Oscar Dominguez <dominguez.celada@gmail.com>
2020-12-26 22:31:13 +0100

    fix(puppeteer): process exit 1 when execution fails

--- src/index.js
+++ src/index.js
@@ -7,19 +7,23 @@ async function main () {
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   })
   const page = await browser.newPage()
-  
-  await page.goto(`https://${process.env.COMPANY}.bamboohr.com/login.php`)
-  await page.click('.normal-login-link-container')
-  await page.type('#lemail', process.env.USER)
-  await page.type('input[type="password"]', process.env.PASSWORD)
-  const [response] = await Promise.all([
-    page.waitForNavigation(),
-    page.click('[type="submit"]')
-  ])
-  console.log(response)
-  await page.type('.TimeTrackingWidget__form input', '8')
-  await page.click('.js-save-timesheet-button-wrap button')
-  browser.close()
+  try {
+    await page.goto(`https://${process.env.COMPANY}.bamboohr.com/login.php`)
+    await page.click('.normal-login-link-container')
+    await page.type('#lemail', process.env.USER)
+    await page.type('input[type="password"]', process.env.PASSWORD)
+    const [response] = await Promise.all([
+      page.waitForNavigation(),
+      page.click('[type="submit"]')
+    ])
+    console.log(response)
+    await page.type('.TimeTrackingWidget__form input', '8')
+    await page.click('.js-save-timesheet-button-wrap button')
+    browser.close()
+  } catch (error) {
+    console.error(error)
+    process.exit(1);
+  }
 }
 
 main()
