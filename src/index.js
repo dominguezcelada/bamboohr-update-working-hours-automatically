1ff5247744acbcc1d29c7da62291dbe94cbae9ae
Oscar Dominguez <dominguez.celada@gmail.com>
2020-12-26 22:23:44 +0100

    fix(puppeteer): remove try-catch to make workflow fail when execution fails

--- src/index.js
+++ src/index.js
@@ -7,23 +7,19 @@ async function main () {
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   })
   const page = await browser.newPage()
-  try {
-    await page.goto(`https://${process.env.COMPANY}.bamboohr.com/login.php`)
-    await page.click('.normal-login-link-container')
-    await page.type('#lemail', process.env.USER)
-    await page.type('input[type="password"]', process.env.PASSWORD)
-    const [response] = await Promise.all([
-      page.waitForNavigation(),
-      page.click('[type="submit"]')
-    ])
-    console.log(response)
-    await page.type('.TimeTrackingWidget__form input', '8')
-    await page.click('.js-save-timesheet-button-wrap button')
-  } catch (error) {
-    console.error(error)
-  } finally {
-    browser.close()
-  }
+  
+  await page.goto(`https://${process.env.COMPANY}.bamboohr.com/login.php`)
+  await page.click('.normal-login-link-container')
+  await page.type('#lemail', process.env.USER)
+  await page.type('input[type="password"]', process.env.PASSWORD)
+  const [response] = await Promise.all([
+    page.waitForNavigation(),
+    page.click('[type="submit"]')
+  ])
+  console.log(response)
+  await page.type('.TimeTrackingWidget__form input', '8')
+  await page.click('.js-save-timesheet-button-wrap button')
+  browser.close()
 }
 
 main()
