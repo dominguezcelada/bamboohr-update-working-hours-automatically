5c7e99f59723e2b24bb26439f852ec1bbafcf483
Oscar Dominguez <dominguez.celada@gmail.com>
2021-01-02 00:12:29 +0100

    style(lint): aaplied lint fixes to repo

--- src/bamboo-hr-interactions/login/index.ts
+++ src/bamboo-hr-interactions/login/index.ts
@@ -1,24 +1,21 @@
-import { Page } from "puppeteer";
+import { Page } from 'puppeteer';
 
 const checkEnvironmentVariables = () => {
-  ["COMPANY", "USER", "PASSWORD"].forEach((envVariable) => {
-    if (!process.env[envVariable]) {
-      throw new Error(`Environment variable ${envVariable} not defined`);
-    }
-  });
+    ['COMPANY', 'USER', 'PASSWORD'].forEach((envVariable) => {
+        if (!process.env[envVariable]) {
+            throw new Error(`Environment variable ${envVariable} not defined`);
+        }
+    });
 };
 
 export const login = async (page: Page) => {
-  checkEnvironmentVariables();
+    checkEnvironmentVariables();
 
-  await page.goto(`https://${process.env.COMPANY}.bamboohr.com/login.php`);
-  await page.click(".normal-login-link-container");
-  await page.type("#lemail", process.env.USER as string);
-  await page.type('input[type="password"]', process.env.PASSWORD as string);
-  const [response] = await Promise.all([
-    page.waitForNavigation(),
-    page.click('[type="submit"]'),
-  ]);
+    await page.goto(`https://${process.env.COMPANY}.bamboohr.com/login.php`);
+    await page.click('.normal-login-link-container');
+    await page.type('#lemail', process.env.USER as string);
+    await page.type('input[type="password"]', process.env.PASSWORD as string);
+    const [response] = await Promise.all([page.waitForNavigation(), page.click('[type="submit"]')]);
 
-  return response;
+    return response;
 };
